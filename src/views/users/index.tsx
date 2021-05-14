7ca8d8e83008014008e2d9c9a5e92f562c70200d
gaoliver <gabriel.o.c.ramos@gmail.com>
2021-05-14 20:20:19 -0300

    Users page

--- src/views/users/index.tsx
+++ src/views/users/index.tsx
@@ -1,46 +1,50 @@
-import "./index.scss";
-import { Table } from "reactstrap";
 import { MdCheckBox, FaTrash } from "react-icons/all";
+import "./index.scss";
 
 function Users() {
-  const approve = <MdCheckBox color="green" size="1.3rem" />;
-  const remove = <FaTrash color="red" />;
   return (
     <div className="view-content">
-      <Table responsive>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            <td>{approve} - {remove}</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-            <td>{remove}</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-            <td>{approve} - {remove}</td>
-          </tr>
-        </tbody>
-      </Table>
+      <div className="table-responsive">
+        <table className="table align-middle">
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>markotto@site.com</td>
+              <td id="actions">
+                <MdCheckBox color="green" size="1.3rem" />
+                <FaTrash color="red" />
+              </td>
+            </tr>
+            <tr>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>jacobthornton@site.com</td>
+              <td id="actions">
+                <MdCheckBox color="green" size="1.3rem" />
+                <FaTrash color="red" />
+              </td>
+            </tr>
+            <tr>
+              <td>Larry</td>
+              <td>the Bird</td>
+              <td>larry_thebird@site.com</td>
+              <td id="actions">
+                <MdCheckBox color="green" size="1.3rem" />
+                <FaTrash color="red" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
